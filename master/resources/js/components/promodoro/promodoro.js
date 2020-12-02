f219232a8eb47a580db83dc79ce75260accbf4e1
Thanhhff <thanhnguyen200399@gmail.com>
2020-12-02 13:11:15 +0700

    Remake Promodoro.js

--- master/resources/js/components/promodoro/promodoro.js
+++ master/resources/js/components/promodoro/promodoro.js
@@ -1,7 +1,7 @@
-const SESSION_MINUTES= 25;
-const BREAK_MINUTES= 5;
-const LONG_BREAK_MINUTES= 15;
-const TIME_SECONDS= 10;
+const SESSION_MINUTES = 25;
+const BREAK_MINUTES = 5;
+const LONG_BREAK_MINUTES = 15;
+const TIME_SECONDS = 10;
 const PROMODORO_TEMPLATE = [
     'session',
     'break',
@@ -53,12 +53,12 @@ export default {
                 this.promodoroTemplate = JSON.parse(localStorage.getItem('promodoroTemplate'))
             }
             const modes = JSON.parse(localStorage.getItem('modes'));
-            const { session, break: rest, longBreak} = this.modes;
+            const {session, break: rest, longBreak} = this.modes;
             if (modes) {
                 session.minutes = modes.session.minutes;
                 session.seconds = modes.session.seconds;
                 rest.minutes = modes.break.minutes;
-                rest.seconds =   modes.break.seconds;
+                rest.seconds = modes.break.seconds;
                 longBreak.minutes = modes.longBreak.minutes;
                 longBreak.seconds = modes.longBreak.seconds
             }
