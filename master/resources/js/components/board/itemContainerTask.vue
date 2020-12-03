6b7b124f139c5ee83c863ce302a0d103fd8b4014
Thanhhff <thanhnguyen200399@gmail.com>
2020-12-03 11:03:30 +0700

    Update itemContainerTask - Todo

--- master/resources/js/components/board/itemContainerTask.vue
+++ master/resources/js/components/board/itemContainerTask.vue
@@ -1,43 +1,38 @@
 <template>
-<div  class="task-item">
-    <div>
-        <label class="checkbox-label">
-        <input
-            type="checkbox"
-            @change="$emit('update-item', task)"
-            name=""
-            :id="task.id"
-            v-model="task.done"
-        />
-            <span class="font-bold">
+    <div class="task-item">
+        <div>
+            <label class="checkbox-label">
+                <input
+                    type="checkbox"
+                    @change="$emit('update-item', task)"
+                    name=""
+                    :id="task.id"
+                    v-model="task.done"
+                />
+                <span class="font-bold">
             <inertia-link :href="`/boards/${task.board_id}`">
                 [{{ task.stage }}]
             </inertia-link>
         </span>
-        <span>
+                <span>
             {{ task.title }}
         </span>
-        </label>
-    </div>
-    <div class="actions-container flex items-center">
-        <span class="mr-5">
-            {{ priorityText }}
-        </span>
+            </label>
+        </div>
+        <div class="actions-container flex items-center">
             <el-tooltip class="item" effect="dark" :content="task.priority" placement="left">
-                <div class="priority-level inline-block mr-4">
-                    <div class="priority-level__inner">
-
-                    </div>
+                <div class="inline-block mr-5">
+                    {{ priorityText }}
                 </div>
             </el-tooltip>
-        <span class="mr-2">
+            <span class="mr-2">
             {{ durationFromMs }}
         </span>
-        <button @click="$emit('item-clicked', task)" class="play-button" v-if="!task.commit_date">
-            <i class="fa" :class="[isTracker ? 'fa-pause' : 'fa fa-play']"/>
-        </button>
+            <button @click="$emit('item-clicked', task)" class="play-button" v-if="!task.commit_date">
+                <i class="fa" :class="[isTracker ? 'fa-pause' : 'fa fa-play']"/>
+            </button>
+        </div>
     </div>
-</div>
 </template>
 
 <script>
@@ -59,38 +54,17 @@ export default {
             return this.tracker && this.tracker.timeEntry.item_id == this.task.id;
         },
         durationFromMs() {
-            const currentDuration = this.isTracker ? this.tracker.duration || 0: 0;
-            return Tracker.durationFromMs(this.task.duration+currentDuration);
+            const currentDuration = this.isTracker ? this.tracker.duration || 0 : 0;
+            return Tracker.durationFromMs(this.task.duration + currentDuration);
         },
         priorityText() {
             const emojis = {
-                "high": "🔥🔥🔥",
-                "medium": "🔥🔥",
-                "low": "🔥"
+                "high": "🌟🌟🌟️",
+                "medium": "🌟🌟️",
+                "low": "🌟"
             }
             return this.task && emojis[this.task.priority] || "";
         }
     },
 }
 </script>
-
-<style lang="scss" scoped>
-
-.priority-level {
-    --color: red;
-    border: solid 1px transparent;
-    border-radius: 0.20rem;
-    padding: 2px;
-    transition: all ease .3s;
-
-    &:hover {
-        border: solid 1px var(--color);
-    }
-    &__inner {
-        background-color: var(--color);
-        width: 12px;
-        height: 12px;
-        border-radius: 0.20rem;
-    }
-}
-</style>
