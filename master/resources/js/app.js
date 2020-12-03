f8af204f67b20e92c2db3e621e6fe9e956b08af1
Thanhhff <thanhnguyen200399@gmail.com>
2020-12-03 14:50:32 +0700

    Update app.js

--- master/resources/js/app.js
+++ master/resources/js/app.js
@@ -3,9 +3,6 @@ require('vue-multiselect/dist/vue-multiselect.min.css')
 
 import Vue from 'vue';
 
-import BootstrapVue from 'bootstrap-vue' //Importing
-Vue.use(BootstrapVue) // Telling Vue to use this in whole application
-
 import {InertiaApp} from '@inertiajs/inertia-vue';
 import {InertiaForm} from 'laravel-jetstream';
 import ConfirmModalMixin from "./plugins/ConfirmModalMixin";
